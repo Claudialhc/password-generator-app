1eee8527c8c7192879eb4a48d9529bc1bfbdb12a
Claudia Hernandez <claudiahdez13@gmail.com>
2020-12-10 17:29:13 -0800

    changed the alerts to confirms and added a return to the last

--- script.js
+++ script.js
@@ -10,33 +10,41 @@ var alpha1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "
 var alpha2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var symbols = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
 var num = "0123456789"
-var empty = []
+var pool = []
 
 for (var i = 0; i < alpha1.length; i++) {
   console.log(alpha1[i]);
 }
-  console.log("----")
+console.log("----")
 
-  
+
 
 function generatePassword() {
-  var length = parseInt(prompt ("How many Characters would you like your password to be? Between 8 and 128?"));
+  //if (useLowercase) {
+  //   pool = pool.concat(lowercase)  
+  //}
+  var length = parseInt(prompt("How many Characters would you like your password to be?"));
   while (length < 8 || user < 128) {
     alert("OPPS! Must be between 8 and 128 characters.")
-        length = parseInt(prompt ("How many Characters would you like your password to be? Between 8 and 128?"));
-  }   
+    length = parseInt(prompt("How many Characters would you like your password to be?"));
+  }
   for (var i = 0; i < length; i++) {
-     length.push = (alpha1[Math.floor(Math.random)] * alpha1); //originally was 2 x26 instead of alpha1 change if needed
-      if (length < 8 || user > 128) {
-          alert("OPPS! Must be between 8 and 128 characters.");
-  }   if (length > 8 || user < 128) {
-          alert("Click ok to include special characters?");
-  }   if ("Click ok to include special charactera?") { //how do I get this one to loop in ? another while loop?
-          return alert("Click ok to include numbers?")
-        }
-      }; 
-      //lowercase = ok("click ok to include lowercase?")
-      //uppercase = ok("click ok to include uppercase?")
+    length.push = (alpha1[Math.floor(Math.random)] * alpha1); //originally was 2 x26 instead of alpha1 change if needed -- change alpha1 to
+    if (length < 8 || user > 128) {
+      alert("OPPS! Must be between 8 and 128 characters.");
+    } if (length > 8 || user < 128) {
+      confirm("Click ok to include special characters?");
+    } if ("Click ok to include special characters?") { //how do I get this one to loop in ? another while loop?
+      confirm("Click ok to include numbers?");
+    } if ("Click ok to include numbers?") {
+      confirm("Click ok to include lowercase?");
+    } if ("Click ok to include numbers?") {
+      return confirm("Click ok to inclide uppercase?");
+    }
+  };
+  //return stops the function
+  //lowercase = ok("click ok to include lowercase?")
+  //uppercase = ok("click ok to include uppercase?")
 
   return "password";
 }
