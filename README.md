3ebeedf89420b9d9a5fdc1ca86e3613b7f365b85
Claudia Hernandez <claudiahdez13@gmail.com>
2020-12-10 19:00:11 -0800

    added readne

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-//make a prompt 
-//create a var for length 
-//define character length - has to be between 8 and 128
-//create the for (var i = 0; i < length; i++) {
-    var length = push(Math.floor(Math.random) * 26);
-} 
-//
+This template was to design, using only Javascript, a generate top security random passwords for employees with acces to sensitive adata. The criteria was for the passwords to provide them with greater security. 
+
+When you open the site, you will encounter a box designed to generate random passwords. To get your unique high security password click the "Generate Password" red button. When you click the button you will see pop-up windows at the middle top section of the page. The series of windows that appear depend on how complex you want your password to be. The suggested password criteria are: 
+<ul>
+    <li>How Many Characters do you want?</li>
+    <li>Do you whant special characters?</li>
+    <li>Do you want numeric characters?</li>
+    <li>Do you want uppercase letters?</li>
+    <li>Do you want lowercase letters?</li>
+</ul>
+
+These pop-ups were created with prompts, alerts and confirm javascript elements. There are also for loops and while loops involved to be able to make the prompt, alerts and confirms take de the deisred sequence. 
+
+Unfortunately, I was not able to make the actual random password generator to work. 
--- script.js
+++ script.js
@@ -1,35 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//these are the variables
 var user;
 var uppercase;
 var lowercase;
 var characters;
 var numbers;
+//these are the arrays for that will be needed to generate the random password.
 var alpha1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var alpha2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//I made this array look different to show that you can also use them as strings and it will still work like the above
 var symbols = " !\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"
 var num = "0123456789"
+//this is the empty variable that is suppose to be the glue of it all
 var pool = []
 
-for (var i = 0; i < alpha1.length; i++) {
-  console.log(alpha1[i]);
-}
-console.log("----")
 
 
 
+//here is the function used to generate the password
 function generatePassword() {
-  //if (useLowercase) {
-  //   pool = pool.concat(lowercase)  
-  //}
+  if (alpha2) {
+    pool = pool.concat(alpha2)
+  }
+  //this is the prompt needed to trigger the first window on the page followed by the confirms
   var length = parseInt(prompt("How many Characters would you like your password to be?"));
   while (length < 8 || user < 128) {
     alert("OPPS! Must be between 8 and 128 characters.")
     length = parseInt(prompt("How many Characters would you like your password to be?"));
   }
   for (var i = 0; i < length; i++) {
-    length.push = (alpha1[Math.floor(Math.random)] * alpha1); //originally was 2 x26 instead of alpha1 change if needed -- change alpha1 to
+    length.push = (pool[Math.floor(Math.random)] * pool); //originally was 2 x26 instead of alpha1 change if needed -- change alpha1 to
     if (length < 8 || user > 128) {
       alert("OPPS! Must be between 8 and 128 characters.");
     } if (length > 8 || user < 128) {
@@ -39,12 +41,11 @@ function generatePassword() {
     } if ("Click ok to include numbers?") {
       confirm("Click ok to include lowercase?");
     } if ("Click ok to include numbers?") {
+      //the return infront of the confirm makes the loop stop and lets the generator know it is time to hand out a password
       return confirm("Click ok to inclide uppercase?");
     }
   };
-  //return stops the function
-  //lowercase = ok("click ok to include lowercase?")
-  //uppercase = ok("click ok to include uppercase?")
+
 
   return "password";
 }
@@ -62,3 +63,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
